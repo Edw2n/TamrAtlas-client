64154d043d863e9dbf38d638efc000a0e86eb595
Edw2n <60819319+Edw2n@users.noreply.github.com>
2020-11-22 12:25:37 +0900

    fill tamra grids with a suguk pattern

--- src/components/Atlas.js
+++ src/components/Atlas.js
@@ -22,18 +22,32 @@ function Atlas() {
         var path = d3.geoPath().projection(projection);
 
         // load data
-        var tamraMap = d3.json("https://raw.githubusercontent.com/Edw2n/geojson-prac/master/tamra-square-vanilla.geojson", function (data){
+        var tamraMap = d3.json("https://raw.githubusercontent.com/Edw2n/geojson-prac/master/tamra-square-vanilla.geojson", function (data) {
             console.log(data);
         });
 
         Promise.all([tamraMap]).then(function (values) {
             // draw map
+            svg.append("defs")
+                .append("pattern")
+                .attr("id", "bg")
+                .attr("width","100%")
+                .attr('height',"100%")
+                .attr("patternContentUnits", "objectBoundingBox")
+                .append("image")
+                .attr("height","1") // value is ratio : "image height /pattern height"
+                .attr("width","1") // value is ratio : "image widht /pattern width"
+                .attr("xlink:href", // use square size image
+                    "https://previews.123rf.com/images/taratata/taratata1711/taratata171100020/89494129-%EC%88%98%EA%B5%AD-%EC%88%98%EA%B5%AD-%EA%B1%B0%EB%AF%B8-%EC%95%84%EB%A6%84-%EB%8B%A4%EC%9A%B4-%EB%B3%B4%EB%9D%BC%EC%83%89-%EC%88%98-%EA%B5%AD-%EA%BD%83-%EA%B7%BC%EC%A0%91%EC%9E%85%EB%8B%88%EB%8B%A4-%EA%BD%83%EC%A7%91%EC%97%90-%EC%84%A0%EB%B0%98%EC%9E%85%EB%8B%88%EB%8B%A4-%ED%8F%89%EB%A9%B4%EB%8F%84-.jpg");
+
             svg.selectAll("path")
                 .data(values[0].features)
                 .enter()
                 .append("path")
                 .attr("class", "tamra")
                 .attr("d", path)
+                .style('fill', "url(#bg)");
+
         });
     });
 
