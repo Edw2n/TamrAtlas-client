0e2e68bd43f7b339063a16bcea1784618da9b7fa
Edw2n <60819319+Edw2n@users.noreply.github.com>
2020-11-22 13:45:18 +0900

    support basic zoom in/out and navigate Atlas

--- src/components/Atlas.js
+++ src/components/Atlas.js
@@ -3,10 +3,10 @@ import * as d3 from 'd3';
 import './Atlas.css';
 
 function Atlas() {
+    let w = 2000;
+    let h = 2000;
     React.useEffect(() => {
         // TODO draw the map using d3
-        var w = 2000;
-        var h = 2000;
 
         const svg = d3.select('#mapCanvas');
         svg
@@ -31,16 +31,17 @@ function Atlas() {
             svg.append("defs")
                 .append("pattern")
                 .attr("id", "bg")
-                .attr("width","100%")
-                .attr('height',"100%")
+                .attr("width", "100%")
+                .attr('height', "100%")
                 .attr("patternContentUnits", "objectBoundingBox")
                 .append("image")
-                .attr("height","1") // value is ratio : "image height /pattern height"
-                .attr("width","1") // value is ratio : "image widht /pattern width"
+                .attr("height", "1") // value is ratio : "image height /pattern height"
+                .attr("width", "1") // value is ratio : "image widht /pattern width"
                 .attr("xlink:href", // use square size image
                     "https://previews.123rf.com/images/taratata/taratata1711/taratata171100020/89494129-%EC%88%98%EA%B5%AD-%EC%88%98%EA%B5%AD-%EA%B1%B0%EB%AF%B8-%EC%95%84%EB%A6%84-%EB%8B%A4%EC%9A%B4-%EB%B3%B4%EB%9D%BC%EC%83%89-%EC%88%98-%EA%B5%AD-%EA%BD%83-%EA%B7%BC%EC%A0%91%EC%9E%85%EB%8B%88%EB%8B%A4-%EA%BD%83%EC%A7%91%EC%97%90-%EC%84%A0%EB%B0%98%EC%9E%85%EB%8B%88%EB%8B%A4-%ED%8F%89%EB%A9%B4%EB%8F%84-.jpg");
 
-            svg.selectAll("path")
+            var geo = svg.append('g');
+            geo.selectAll("path")
                 .data(values[0].features)
                 .enter()
                 .append("path")
@@ -48,7 +49,17 @@ function Atlas() {
                 .attr("d", path)
                 .style('fill', "url(#bg)");
 
+            svg.call(d3.zoom()
+                .extent([[0, 0], [w, h]])
+                .scaleExtent([1, 6])
+                .on("zoom", zoomed));
+
+            function zoomed({transform}) {
+                geo.attr("transform", transform);
+            }
+
         });
+
     });
 
     return (
