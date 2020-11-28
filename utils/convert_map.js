d53da4d363e9d8efcd2c85b10596e3c5958e9087
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2020-11-28 18:51:57 +0900

    Add grid_num parameter to map converter

--- utils/convert_map.js
+++ utils/convert_map.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const d3 = require('d3');
 
-function geojson2data(geojson) {
+function geojson2data(geojson, num_grid) {
+  const threshold = {
+    same: 0.0001,
+    adjacent: 0.01 / num_grid
+  };
+
   const coords = geojson.features.map((f) => {
     let latAvg = 0;
     let lonAvg = 0;
@@ -27,30 +32,30 @@ function geojson2data(geojson) {
   onlyJeju.forEach((d, i) => {
     const topIndex = onlyJeju.findIndex((element, j) => {
       return i !== j
-        && Math.abs(element.point[0] - d.point[0]) < 0.0001
+        && Math.abs(element.point[0] - d.point[0]) < threshold.same
         && d.point[1] > element.point[1]
-        && d.point[1] - element.point[1] < 0.01;
+        && d.point[1] - element.point[1] < threshold.adjacent;
     });
 
     const bottomIndex = onlyJeju.findIndex((element, j) => {
       return i !== j
-        && Math.abs(element.point[0] - d.point[0]) < 0.0001
+        && Math.abs(element.point[0] - d.point[0]) < threshold.same
         && d.point[1] < element.point[1]
-        && element.point[1] - d.point[1] < 0.01;
+        && element.point[1] - d.point[1] < threshold.adjacent;
     });
 
     const leftIndex = onlyJeju.findIndex((element, j) => {
       return i !== j
-        && Math.abs(element.point[1] - d.point[1]) < 0.001
+        && Math.abs(element.point[1] - d.point[1]) < threshold.same
         && d.point[0] > element.point[0]
-        && d.point[0] - element.point[0] < 0.01;
+        && d.point[0] - element.point[0] < threshold.adjacent;
     });
 
     const rightIndex = onlyJeju.findIndex((element, j) => {
       return i !== j
-        && Math.abs(element.point[1] - d.point[1]) < 0.001
+        && Math.abs(element.point[1] - d.point[1]) < threshold.same
         && d.point[0] < element.point[0]
-        && element.point[0] - d.point[0] < 0.01;
+        && element.point[0] - d.point[0] < threshold.adjacent;
     });
 
     d.top = topIndex < 0 ? undefined : topIndex;
@@ -62,14 +67,28 @@ function geojson2data(geojson) {
 }
 
 args = process.argv.slice(2);
-if (args.length < 2) {
-  console.log('Usage: node convert_map <geojson_file_to_convert> <new_file_name>');
+if (args.length < 3) {
+  console.log('Usage: node convert_map <num_grid> <geojson_file_to_convert> <new_file_name>');
+  console.log('Supported values for num_grid: 1x, 2x, 4x');
+  process.exit(1)
 }
 
-console.log(`Converting ${args[0]} to ${args[1]}`);
-datastr = fs.readFileSync(args[0], { encoding: 'utf-8', flag: 'r' });
+let num_grid;
+if (args[0] === '1x') {
+  num_grid = 1;
+} else if (args[0] === '2x') {
+  num_grid = 2;
+} else if (args[0] === '4x') {
+  num_grid = 4;
+} else {
+  console.log('Unsuppored value. Supported values for num_grid: 1x, 2x, 4x');
+  process.exit(1)
+}
+
+console.log(`Converting ${args[1]} to ${args[2]}`);
+datastr = fs.readFileSync(args[1], { encoding: 'utf-8', flag: 'r' });
 geo = JSON.parse(datastr);
 
-const data = geojson2data(geo);
+const data = geojson2data(geo, num_grid);
 
-fs.writeFileSync(args[1], JSON.stringify(data));
+fs.writeFileSync(args[2], JSON.stringify(data));
