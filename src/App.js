d12c5ccaf0868056e554ca8581a1a6764cc88af9
kev-damian <maheu@outlook.com>
2020-12-03 01:03:05 +0900

    add searchbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import Atlas from './components/Atlas';
+import SearchBar from './components/SearchBar'
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([])
+  const [negItems, setNegItems] = useState([])
   return (
     <div className="App">
+      <SearchBar items={items} setItems={setItems} negItems={negItems} setNegItems={setNegItems}/>
       <Atlas />
     </div>
   );
